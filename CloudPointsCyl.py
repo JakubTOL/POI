feae483da36bf0c8958de829bcc1b9d625e37415
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-03-14 15:17:55 +0100

    script for cylindric shape

---
+++ CloudPointsCyl.py
@@ -0,0 +1,26 @@
+from scipy.stats import norm #import pakietu rozkładu normalnego
+from csv import writer
+
+
+def generate_points(num_points:int=2000): #definicja fukcji z parametrem domyslnym
+    #definiowanie obiektow z parametrami rozkladu normalnego
+    #x oraz y takie same aby utworzyc podstawe cylindryczna Z przeskalowane aby wyciagnac wysokosc bryly
+    distribution_x = norm(loc=0, scale=20)
+    distribution_y = norm(loc=0, scale=20)
+    distribution_z = norm(loc=0, scale=200)
+
+    x = distribution_x.rvs(size=num_points)
+    y = distribution_y.rvs(size=num_points)
+    z = distribution_z.rvs(size=num_points)
+
+    #zapis punktow do pliku .csv, ale z roszerzeniem .xyz
+    points = zip(x, y, z) #struktura zawierająca wygenerwoane koorydnaty ale 'zip' stworzył krotki wspolrzednych
+    return points
+
+
+if __name__=='__main__':#aby importowac tylko procedure generate_points
+    cloud_points = generate_points(2000)
+    with open('CloudPointsCyl.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
+        csvwriter = writer(csvfile)
+        for p in cloud_points:#iteracja po pakietach punktow 'zip'
+            csvwriter.writerow(p)#zapisz do pliku konkretna krotke
