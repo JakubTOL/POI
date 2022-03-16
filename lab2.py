f4d17b9764f261fd900ff731576e297598c7c7a8
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-03-16 18:14:13 +0100

    need to fix bug with zip'ing values for .csv

--- lab2.py
+++ lab2.py
@@ -1,9 +1,13 @@
 import csv
+from sklearn.cluster import KMeans
+from scipy.stats import norm
 import pyransac3d as pyrsc
+import matplotlib.pyplot as plt
+import numpy as np
 """
 1. odczyt plikow csv \/
 2. instalacja pakiety pyransac3d \/
-3. znalezc rozlaczne chmury punktow za pomoca algorytmu k-srednich k=3 ???
+3. znalezc rozlaczne chmury punktow za pomoca algorytmu k-srednich k=3 ??? --clusteryzacja?
 4. dla kazdejc chmury przeprowadzic dopaswoanie plaszczyzny za pomoca ransac
    4.1. wypisac wspolrzedne wektora normalnego do znalezionej plaszczyzny
     4.1.2 czy dana chmura jest placzczynza
@@ -21,14 +25,20 @@ def point_read_xy():  # definicja funkcji
 
 pointsXY = []  # utworzenie listy
 for p in point_read_xy():
-    pointsXY.append(p)  # wpisywanie wierszy do listy
-    print(p)
-    # points.sort()  # sortowanie listy po pierwszym elemencie kazdej krotki
+    pointsXY.extend(p)  # wpisywanie wierszy do listy
+    # print(p)
+    # pointsXY.sort()  # sortowanie listy po pierwszym elemencie kazdej krotki
     # points.sort(key=lambda x: x[n]) #sortowanie po n-tym elemencie kazdej krotki lambda jest funkcja anonimowa
-pointsXY
+x1, y1, z1 = zip(*pointsXY)  # wydziel wartosci z tablicy pointsXY  # TU JEST BLAD OCZYTU
+print(x1)
+"""plt.figure()
+plt.ylabel('x', fontsize=12)
+plt.xlabel('y', fontsize=12)
+plt.tight_layout()
+plt.scatter(x1, y1)
+plt.show()"""
 
-
-def point_read_xz():  # definicja funkcji
+"""def point_read_xz():  # definicja funkcji
     with open('CloudPointsXZ.xyz', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         # next(reader)  # pominiecie naglowka
@@ -39,8 +49,6 @@ def point_read_xz():  # definicja funkcji
 pointsXZ = []  # utworzenie listy
 for p in point_read_xz():
     pointsXZ.append(p)  # wpisywanie wierszy do listy
-    print(p)
-pointsXZ
 
 
 def point_read_cyl():  # definicja funkcji
@@ -53,6 +61,9 @@ def point_read_cyl():  # definicja funkcji
 
 pointsCyl = []  # utworzenie listy
 for p in point_read_cyl():
-    pointsCyl.append(p)  # wpisywanie wierszy do listy
-    print(p)
-pointsCyl
+    pointsCyl.append(p)  # wpisywanie wierszy do listy"""
+
+"""cluster = KMeans(n_clusters=3)  # stworznie obiektu cluster n_cluster--- liczba clastrow
+X = np.array(clusters)
+cluster.fit(X)
+y_pred = cluster.fit_predict(X)  # rozpoznaj cluster i dopasuj sie do niego"""
