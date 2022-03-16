bb60e57089a4ba8d96300ee0448a4a24d43838ef
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-03-16 16:33:46 +0100

    parsing of .xyz(.csv) file

--- lab2.py
+++ lab2.py
@@ -1,18 +1,18 @@
 import csv
 
 
-def planet_reader():  # definicja funkcji
-    with open('planets.csv', newline='') as csvfile:
+def point_read():  # definicja funkcji
+    with open('CloudPointsXY.xyz', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        next(reader)  # poiniecie naglowka
-        for name, seq, radius, mass in reader:  # wprowadzenie nazw naglowkow
-            yield (name, int(seq), int(radius), float(mass))  # odczytuj i zapamietuj wartosci z tych kolumn
+        #next(reader)  # poiniecie naglowka
+        for x, y, z in reader:  # wprowadzenie nazw naglowkow
+            yield x, y, z  # odczytuj i zapamietuj wartosci z tych kolumn
 
 
-planets = []  # utworzenie listy
-for p in planet_reader():
-    planets.append(p)  # wpisywanie wierszy do listy
-
-planets.sort()  # sortowanie listy po pierwszym elemencie kazdej krotki
-#planets.sort(key=lambda x: x[n]) #sortowanie po n-tym elemencie kazdej krotki lambda jest funkcja anonimowa
-planets
+points = []  # utworzenie listy
+for p in point_read():
+    points.append(p)  # wpisywanie wierszy do listy
+    print(p)
+    # points.sort()  # sortowanie listy po pierwszym elemencie kazdej krotki
+    # points.sort(key=lambda x: x[n]) #sortowanie po n-tym elemencie kazdej krotki lambda jest funkcja anonimowa
+points
