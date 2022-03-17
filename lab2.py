1e6a29ec39e7f0c578dfe9763a40676cbcc7ca07
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-03-17 22:41:09 +0100

    tip for ransac alg

--- lab2.py
+++ lab2.py
@@ -1,8 +1,8 @@
 import csv
 from sklearn.cluster import KMeans
-import pyransac3d as pyrsc
 import matplotlib.pyplot as plt
 import numpy as np
+import pyransac3d as pyrsc
 """
 1. odczyt plikow csv \/
 2. instalacja pakiety pyransac3d \/
@@ -12,6 +12,44 @@ import numpy as np
     4.1.2 czy dana chmura jest placzczynza
     4.1.2 jesli jest to czy jest pionowa czy pozioma
 """
+"""Given:
+    data – A set of observations.
+    model – A model to explain observed data points.
+    n – Minimum number of data points required to estimate model parameters.
+    k – Maximum number of iterations allowed in the algorithm.
+    t – Threshold value to determine data points that are fit well by model.
+    d – Number of close data points required to assert that a model fits well to data.
+
+Return:
+    bestFit – model parameters which best fit the data (or null if no good model is found)
+
+iterations = 0
+bestFit = null
+bestErr = something really large
+
+while iterations < k do
+    maybeInliers := n randomly selected values from data
+    maybeModel := model parameters fitted to maybeInliers
+    alsoInliers := empty set
+    for every point in data not in maybeInliers do
+        if point fits maybeModel with an error smaller than t
+             add point to alsoInliers
+        end if
+    end for
+    if the number of elements in alsoInliers is > d then
+        // This implies that we may have found a good model
+        // now test how good it is.
+        betterModel := model parameters fitted to all points in maybeInliers and alsoInliers
+        thisErr := a measure of how well betterModel fits these points
+        if thisErr < bestErr then
+            bestFit := betterModel
+            bestErr := thisErr
+        end if
+    end if
+    increment iterations
+end while
+
+return bestFit"""
 
 
 def point_read_xy():  # definicja funkcji
@@ -43,8 +81,3 @@ if __name__ == '__main__':
     plt.scatter(X[cyan, 0], X[cyan, 1], c="c")
     plt.tight_layout()
     plt.show()
-
-    points = X
-    plane1 = pyrsc.Plane()
-    best_eq, best_inliers = plane1.fit(points, 0.01)
-    print(best_eq, best_inliers)
