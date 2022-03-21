a5191a2163d812b993c09acd1720940ed4dd7906
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-03-21 13:33:09 +0100

    select random row and calc D point

--- lab2.py
+++ lab2.py
@@ -53,7 +53,7 @@ return bestFit"""
 
 
 def point_read_xy():  # definicja funkcji
-    with open('CloudPointsXY.xyz', newline='') as csvfile:
+    with open('CloudPointsCyl.xyz', newline='') as csvfile:
         # odczyt (typ pliku, separator, typ danych - tutaj zeny byly jako float)
         reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
         # next(reader)  # pominiecie naglowka
@@ -61,14 +61,10 @@ def point_read_xy():  # definicja funkcji
             yield x, y, z  # odczytuj i zapamietuj wartosci z tych kolumn
 
 
-if __name__ == '__main__':
-    clusters = []  # utworzenie listy
-    for cluster in point_read_xy():
-        clusters.extend(cluster)  # wpisywanie wierszy do listy
-
+def klasteryzacja():
     clusterer = KMeans(n_clusters=3)  # stworznie obiektu cluster n_cluster--- liczba clastrow
-    X = np.array(clusters).reshape((2000, 3))  # konwersja macierzy odczytanej 1D do 3D
-    X = np.delete(X, 2, axis=1)  # usun 3 kolumne czyli wspolrzedne z = 0
+    odczyt = np.array(clusters).reshape((2000, 3))  # konwersja macierzy odczytanej 1D do 3D
+    X = np.delete(odczyt, 2, axis=1)  # usun 3 kolumne czyli wspolrzedne z = 0
     y_pred = clusterer.fit_predict(X)  # rozpoznaj cluster i dopasuj sie do niego
 
     red = y_pred == 0  # jesli przydzielone do clustra 0 to czerwone
@@ -81,3 +77,21 @@ if __name__ == '__main__':
     plt.scatter(X[cyan, 0], X[cyan, 1], c="c")
     plt.tight_layout()
     plt.show()
+
+# wybor losowego wiersza z tablicy jako wektory
+    va = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
+    vb = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
+    vc = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
+    vec_ua = va/np.linalg.norm(va)
+    vec_ub = vb/np.linalg.norm(vb)
+    vec_uc = np.cross(vec_ua, vec_ub)
+    w = vec_uc
+    D = -np.sum(np.multiply(w, vc))
+    print(D)
+
+
+if __name__ == '__main__':
+    clusters = []  # utworzenie listy
+    for cluster in point_read_xy():
+        clusters.extend(cluster)  # wpisywanie wierszy do listy
+    klasteryzacja()
