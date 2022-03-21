8a8e7f11d1784746013319f01986c8a4aa9d7dcc
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-03-21 22:23:00 +0100

    Nie zrozumiala przycznya bledu

--- lab2.py
+++ lab2.py
@@ -2,11 +2,12 @@ import csv
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
-import pyransac3d as pyrsc
+import sklearn.linear_model as slm
+#import pyransac3d as pyrsc
 
 
 def point_read_xy():  # definicja funkcji
-    with open('CloudPointsXY.xyz', newline='') as csvfile:
+    with open('CloudPointsCyl.xyz', newline='') as csvfile:
         # odczyt (typ pliku, separator, typ danych - tutaj zeny byly jako float)
         reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
         # next(reader)  # pominiecie naglowka
@@ -14,7 +15,7 @@ def point_read_xy():  # definicja funkcji
             yield x, y, z  # odczytuj i zapamietuj wartosci z tych kolumn
 
 
-def klasteryzacja():
+def klasteryzacja_ransac():
     clusterer = KMeans(n_clusters=3)  # stworznie obiektu cluster n_cluster--- liczba clastrow
     odczyt = np.array(clusters).reshape((2000, 3))  # konwersja macierzy odczytanej 1D do 3D
     X = np.delete(odczyt, 2, axis=1)  # usun 3 kolumne czyli wspolrzedne z = 0
@@ -33,25 +34,45 @@ def klasteryzacja():
     plt.ylabel("y")
     plt.show()
 
-# wybor losowego wiersza z tablicy jako wektory
-    va = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
-    vb = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
-    vc = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
-    vec_ua = va/np.linalg.norm(va)
-    vec_ub = vb/np.linalg.norm(vb)
-    vec_uc = np.cross(vec_ua, vec_ub)
-
-    w = vec_uc
-    D = -np.sum(np.multiply(w, vc))
-    print("odleglosc od 0xyz:", D)
     threshold = 2
-    dist = (w * odczyt + D)/(np.linalg.norm(w))
-    inliers = np.where(np.abs(dist) <= threshold)
-    model_size = len(inliers)
+    max_iter = 100
+    max_inliers_count = 0
+    best = []
+
+    for iteration in range(0, max_iter):
+        # etap 1
+        # wybor losowego wiersza z tablicy jako wektory
+        va = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
+        vb = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
+        vc = odczyt[np.random.choice(odczyt.shape[0], 1, replace=False), :]
+        vec_ua = va/np.linalg.norm(va)
+        vec_ub = vb/np.linalg.norm(vb)
+        vec_uc = np.cross(vec_ua, vec_ub)
+        w = vec_uc
+        D = -np.sum(np.multiply(w, vc))
+        # print("odleglosc od 0xyz:", D)
+        # etap 2
+        inliers_count = 0
+        for i in range(0, len(odczyt)):
+            dist = (w * odczyt + D)/(np.linalg.norm(w))  # odleglosc od plaszczyny
+
+            if np.any(dist) < threshold:
+                inliers_count += 1
+
+            if inliers_count > max_inliers_count:
+                best = [va, vb, vc]
+                max_inliers_count = inliers_count
+
+    print(best)
+    """regresja = slm.LinearRegression()
+    regresja.fit(best, X)
+    print('Wyraz wolny jest równy: %s' % round(regresja.intercept_, 3))
+    print('Współczynnik kierunkowy wynosi: %s' % np.round(regresja.coef_, 3))
+    print('Współczynnik dopasowania wynosi: %s' % round(regresja.score(best, X), 3))"""
 
 
 if __name__ == '__main__':
     clusters = []  # utworzenie listy
     for cluster in point_read_xy():
         clusters.extend(cluster)  # wpisywanie wierszy do listy
-    klasteryzacja()
+    klasteryzacja_ransac()
