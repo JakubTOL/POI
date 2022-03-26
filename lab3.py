a931f5ce3759f745b235c51fe959a918e36937d6
Jakub Adamczyk <222684@edu.p.lodz.pl>
2022-03-26 11:50:38 +0100

    crop, grey_scale & brightness

--- lab3.py
+++ lab3.py
@@ -1,7 +1,9 @@
 # błąd e= l.błędnych klasygikacji/l.wszystkich wektorów
 # dokładność = 1-e  miara działania klasyfikatora
 
-from skimage import io
+from skimage import io, exposure  # odczyt zapis plikow, jasnosc
+from skimage.color import rgb2gray  # do konwersji do skali szarosci
+import pandas
 
 
 # funkcja podzialu dla gresu
@@ -70,9 +72,29 @@ def crop_drewno(wysokosc1, wysokosc2, szerokosc1, szerokosc2):
             szerokosc2 += 128
 
 
+def odczyt_drewno_grey_scale():
+
+    # okreslic dissimilarity, correlation, constrast, energy, homogenity, ASM
+    # odleglosci pikseli 1, 3, 5
+    # 4 kierunki 0, 45, 90, 135 stopni
+    # kazdy wektor uzupelnic o nazwe kategorii tekstury
+    # zbior wektorow zapiac do csv
+
+    # odczyt probek
+    num = 0
+    filepath = "D:/new/drewno/drewno_crop" + str(num) + ".jpg"
+    img = io.imread(filepath)
+    # scikit-image transf do szarosci i glebia jasnosci do 5 bitow:
+    img_gray = rgb2gray(img)  # konwersja do skali szarosci
+    img_brightness = exposure.adjust_gamma(img_gray, gamma=5, gain=1)  # gamma okresla poziom jasnosci
+    io.imshow(img_brightness)  # wyswietlenie dal debugowania co wyszlo
+    io.show()
+
+
 if __name__ == '__main__':
     # funkcje do odczzytu zdjeciea z folderu i podzialu na mniejsze fragmenty
-    crop_gres(0, 128, 0, 128)  # wywołanie funkcji z parametrami wymiarow [od_y1:do_y2, od_x1:do_x2]
-    crop_cegla(0, 128, 0, 128)
-    crop_drewno(0, 128, 0, 128)
-    #
+    # crop_gres(0, 128, 0, 128)  # wywołanie funkcji z parametrami wymiarow [od_y1:do_y2, od_x1:do_x2]
+    # crop_cegla(0, 128, 0, 128)
+    # crop_drewno(0, 128, 0, 128)
+    # funkcja odczytu
+    odczyt_drewno_grey_scale()
